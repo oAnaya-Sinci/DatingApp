0ac6ad11e55721da9f2e6acd825d6f45842daaf4
Omar Anaya <canaya@sinci.com>
2021-05-12 12:17:21 -0500

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,33 +2,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+  [Authorize]
   public class UsersController : BaseApiController
   {
-    private readonly DataContext _context;
-    public UsersController(DataContext context)
+    private readonly IMapper _mapper;
+
+    private readonly IUserRepository _userRepository;
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
-      _context = context;
+      _mapper = mapper;
+      _userRepository = userRepository;
     }
 
     [HttpGet]
-    [AllowAnonymous]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-        
-        return await _context.Users.ToListAsync();
+    public async Task<ActionResult<IEnumerable<MembersDto>>> GetUsers()
+    {
+      var users = await _userRepository.GetMembersAsync();
+
+      return Ok(users);
     }
 
-    [Authorize]
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(int id){
+    // [HttpGet("{id}")]
+    // public async Task<ActionResult<AppUser>> GetUser(int id){
 
-        return await _context.Users.FindAsync(id);
+    //     return await _userRepository.GetUserByUsernameAsync(username);
+    // }
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MembersDto>> GetUser(string username)
+    {
+      return await _userRepository.GetMemberAsync(username);
     }
   }
 }
---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+// using API.Helpers;
+using API.Interfaces;
+using AutoMapper.QueryableExtensions;
+using AutoMapper;
+// using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class UserRepository : IUserRepository
+  {
+    private readonly IMapper _mapper;
+
+    private readonly DataContext _context;
+    public UserRepository(DataContext context, IMapper mapper)
+    {
+      _mapper = mapper;
+      _context = context;
+    }
+
+    public async Task<MembersDto> GetMemberAsync(string username)
+    {
+      return await _context.Users
+            .Where(x => x.UserName == username)
+            .ProjectTo<MembersDto>(_mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<MembersDto>> GetMembersAsync()
+    {
+      return await _context.Users.ProjectTo<MembersDto>(_mapper.ConfigurationProvider).ToListAsync();
+    }
+
+    public async Task<AppUser> GetUserByIdAsync(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<AppUser> GetUserByUsernameAsync(string username)
+    {
+      return await _context.Users
+          .Include(p => p.Photos)
+          .SingleOrDefaultAsync(x => x.UserName == username);
+    }
+
+    public async Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+      return await _context.Users
+          .Include(p => p.Photos)
+          .ToListAsync();
+    }
+
+    public async Task<bool> SaveAllAsync()
+    {
+      return await _context.SaveChangesAsync() > 0;
+    }
+
+    public void Update(AppUser user)
+    {
+      _context.Entry(user).State = EntityState.Modified;
+    }
+  }
+}
